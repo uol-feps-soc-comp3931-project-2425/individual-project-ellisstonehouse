c667fa28db60a614224bac503810186e8fbed9d1
Ellis <sc22es@leeds.ac.uk>
2025-04-18 19:00:42 +0100

    also DDPG param tests

--- ddpg_train_1.py
+++ ddpg_train_1.py
@@ -1,36 +1,29 @@
 import numpy as np
-from maddpg.maddpg import MADDPG
-from maddpg.buffer import MultiAgentReplayBuffer
+from ddpg.agent import Agent
 from env.env import BritishBulldogEnv
 import os
 
 
-BULLDOG = 1
-RUNNER = 0
-MADDPG_ = 0
-RANDOM_ = 1
+# BULLDOG = 1
+# RUNNER = 0
+# DDPG_ = 0
+# RANDOM_ = 1
 
-EPISODES = 10_000
-MAX_STEPS = 500
-PRINT_INTERVAL = 100
+# EPISODES = 10_000
+# MAX_STEPS = 500
+# PRINT_INTERVAL = 100
 
-ALPHA = 1e-3
-BETA = 1e-3
-GAMMA = 0.95
-TAU = 0.01
+# ALPHA = 1e-3
+# BETA = 1e-3
+# GAMMA = 0.95
+# TAU = 0.01
 
-bulldog_algo = MADDPG_
-runner_algo = MADDPG_
+# bulldog_algo = DDPG_
+# runner_algo = DDPG_
 
-model = 'model_1'
-os.makedirs('results/DDPG/'+model, exist_ok=True)
+# model = 'model_1'
+# os.makedirs('results/DDPG/'+model, exist_ok=True)
 
-
-def obs_list_to_state_vector(observation):
-    state = np.array([])
-    for obs in observation:
-        state = np.concatenate([state, obs])
-    return state
 
 def run():
 
@@ -44,24 +37,19 @@ def run():
     episodes = []
 
 
-    actor_dims = []
-    n_actions = []
-    for i in range(env.n_agents):
-        actor_dims.append(env.observation_space[i])
-        n_actions.append(env.action_space[i])
-    critic_dims = sum(actor_dims) + sum(n_actions)
+    agents = []
 
+    for agent_idx in range(env.n_agents):
+        input_dims = env.observation_space[0]
+        n_actions = env.action_space[0]
 
-    maddpg_agents = MADDPG(actor_dims, critic_dims, env.n_agents, n_actions,
-                           gamma=GAMMA, alpha=ALPHA, beta=BETA, tau=TAU,
-                           model=model)
+        agents.append(Agent(agent_idx, alpha=ALPHA, beta=BETA,
+                      input_dims=input_dims, tau=TAU, gamma=GAMMA,
+                      batch_size=1024, fc1_dims=64, fc2_dims=64,
+                      n_actions=n_actions, model=model))
+        
 
-    critic_dims = sum(actor_dims)
-    memory = MultiAgentReplayBuffer(1_000_000, critic_dims, actor_dims,
-                                    n_actions, env.n_agents, batch_size=1024)
-    
-
-    for episode in range(EPISODES+1):
+    for episode in range(EPISODES):
         roles = [BULLDOG]*env.init_bulldogs + [RUNNER]*env.init_runners
         observation = env.reset()
         done = [False]*env.n_agents
@@ -71,53 +59,53 @@ def run():
         runner_score = 0
 
         while not all(done):
-
-            # eval false includes noise for better exploration
-            actions = maddpg_agents.choose_action(observation, evaluate=False)
             
-            # for training against random roles
+            # eval false includes noise for better exploration
+            actions = [agent.choose_action(observation[idx], eval=False)
+                       for idx, agent in enumerate(agents)]
+
+            # randomise role actions for optimal policy
             for idx in range(len(actions)):
                 if roles[idx] == BULLDOG and bulldog_algo == RANDOM_:
                     actions[idx] = np.random.uniform(-1.0, 1.0, size=2)
                 elif roles[idx] == RUNNER and runner_algo == RANDOM_:
                     actions[idx] = np.random.uniform(-1.0, 1.0, size=2)
-
+            
             roles, observation_, rewards, done = env.step(actions)
 
-            state = obs_list_to_state_vector(observation)
-            state_ = obs_list_to_state_vector(observation_)
-
             # store experience
-            memory.store_transition(observation, state, actions, rewards,
-                        observation_, state_, done)
-
+            for idx, agent in enumerate(agents):
+                agent.remember(observation[idx], actions[idx],
+                               rewards[idx], observation_[idx], done[idx])
+            
             # end episode if max steps reached
             if episode_step >= MAX_STEPS:
                 done = [True]*env.n_agents
-
+            
             # every 100 steps learn
             if total_steps % 100 == 0:
-                maddpg_agents.learn(memory)
+                for agent in agents:
+                    agent.learn()
 
             observation = observation_
 
             scores += rewards
-            
+
             for idx, role in enumerate(roles):
                 if role == BULLDOG:
                     bulldog_score += rewards[idx]
                 else: # Runner
                     runner_score += rewards[idx]
-            
+
             total_steps += 1
             episode_step += 1
-        
+
         scores_history.append(scores)
 
         # average agent scores for last 100 episodes
         avg_agent_scores = np.mean(scores_history[-100:], axis=0)
 
-        for idx, agent in enumerate(maddpg_agents.agents):
+        for idx, agent in enumerate(agents):
             if avg_agent_scores[idx] > best_agent_scores[idx]:
                 agent.save_models()
                 best_agent_scores[idx] = avg_agent_scores[idx]
@@ -130,15 +118,39 @@ def run():
             bulldog_avg_score = np.mean(bulldog_score_history[-100:])
             runner_avg_score = np.mean(runner_score_history[-100:])
             print(f'Episode {episode}, last 100 avg, bd score {bulldog_avg_score:.1f}, r score {runner_avg_score:.1f}')
-
-    if bulldog_algo == MADDPG_:
-        np.save('results/MADDPG/'+model+'/bulldogs.npy', np.array(bulldog_score_history))
-    if runner_algo == MADDPG_:
-        np.save('results/MADDPG/'+model+'/runners.npy', np.array(runner_score_history))
-    np.save('results/MADDPG/'+model+'/eps.npy', np.array(episodes))
+        
+    if bulldog_algo == DDPG_:
+        np.save('results/DDPG/'+model+'/bulldogs.npy', np.array(bulldog_score_history))
+    if runner_algo == DDPG_:
+        np.save('results/DDPG/'+model+'/runners.npy', np.array(runner_score_history))
+    np.save('results/DDPG/'+model+'/eps.npy', np.array(episodes))
 
     env.close()
 
 
 if __name__ == '__main__':
-    run()
+
+    BULLDOG = 1
+    RUNNER = 0
+    DDPG_ = 0
+    RANDOM_ = 1
+
+    EPISODES = 5_000
+    MAX_STEPS = 500
+    PRINT_INTERVAL = 100
+
+    GAMMA = 0.95
+    TAU = 0.01
+
+
+    for bulldog_algo in [DDPG_, RANDOM_]:
+        runner_algo = not(bulldog_algo)
+        for ALPHA in [1e-3, 1e-4]:
+            for BETA in [1e-3, 1e-4]:
+
+                model = 'BD_'*(not bulldog_algo)+'R_'*(not runner_algo)+'a='+str(ALPHA)+'_b='+str(BETA)+'_g='+str(GAMMA)+'_t='+str(TAU)
+
+                os.makedirs('results1/DDPG/'+model, exist_ok=True)
+                print(model)
+
+                run()
